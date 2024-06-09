caa568a30715fcfc471eeafa73fcfdcae589043d
MiaoFeng <miao.feng@outlook.de>
2024-06-09 12:10:48 +0800

    Configuration for depolyment on Tecent Cloud

--- server.py
+++ server.py
@@ -258,9 +258,10 @@ def internal_server_error(error):
     return jsonify({'code': 500,'data': [],'msg': 'Internal Server Error'})
 
 #dev runtime
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8080)
-
-# SCF runtime
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9000)
+    app.run(debug=True, port=8080)
+
+# Deploy on Tecent cloud SCF runtime
+# HTTP 直通函数由于是基于 docker 镜像运行，所以必须监听地址为 0.0.0.0，并且端口为 9000
+# if __name__ == '__main__':
+# app.run(host = '0.0.0.0', port = 9000)
